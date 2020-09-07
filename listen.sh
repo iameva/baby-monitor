d2f312bfa2600f540427041e284bf76aa514487d
David Lewis <duve.duve@gmail.com>
2020-09-06 23:24:24 -0600

    wip

--- listen.sh
+++ listen.sh
@@ -1,16 +1,20 @@
-#!/bin/bash
-ffmpeg -re \
-  -f flv -listen 1 -i rtmp://localhost:2021/live/app \
-  -c:a aac -b:a 64k -ac 1 \
-  -c:v libx264 \
-  -movflags +faststart \
-  -crf 30 -preset veryfast -g 30 -sc_threshold 0 \
-  -f hls -hls_time 1 \
-  -hls_flags independent_segments \
-  -hls_list_size 0 \
-  -hls_allow_cache 0 \
-  -hls_segment_type mpegts \
-  -streaming true \
-  -hls_playlist_type event \
-  /www/monitor/video/stream.m3u8
+#!/bin/sh
+ffmpeg \
+  -f rtp -i rtp://localhost:2021/testing
+  -c:a aac -b:a 24k -ac 1 \
+  -c:v libx264 -profile:v baseline \
+  -analyzeduration 0 \
+  -probesize 32 \
+  -level 3.0 -pix_fmt yuv420p \
+  -crf 23 -preset veryfast -g 30 -sc_threshold 0 \
+  -use_template 1 \
+  -seg_duration 1 \
+  -frag_duration 1 \
+  -movflags faststart \
+  -streaming 1 \
+  -master_pl_name stream.m3u8 \
+  -hls_playlist 1 \
+  -master_m3u8_publish_rate 1 \
+  -f dash \
+  /www/monitor/video/stream.mpd
 
--- record-dash.sh
+++ record-dash.sh
@@ -2,30 +2,19 @@
 ffmpeg \
   -f video4linux2 -framerate 30 -video_size 640x480 -i /dev/video0 \
   -f alsa -ac 2 -i hw:1 \
-  -c:a aac -b:a 24k -ac 1 \
+  -c:a aac -b:a 32k -ac 1 \
   -c:v libx264 -profile:v baseline \
   -analyzeduration 0 \
-  -probesize 32 \
+  -probesize  10240 \
   -level 3.0 -pix_fmt yuv420p \
+  -streaming 1 \
+  -index_correction 1 \
   -crf 23 -preset veryfast -g 30 -sc_threshold 0 \
   -use_template 1 \
   -seg_duration 1 \
-  -frag_duration 1 \
-  -movflags faststart \
-  -streaming 1 \
-  -master_pl_name stream.m3u8 \
   -hls_playlist 1 \
+  -master_pl_name stream.m3u8 \
   -master_m3u8_publish_rate 1 \
   -f dash \
   /www/monitor/video/stream.mpd
-
-#ffmpeg -re \
-#  -f alsa -ac 2 -i hw:1 \
-#  -f video4linux2 -framerate 30 -video_size 600x480 -i /dev/video0 \
-#  -c:a libvorbis -b:a 128k -ac 1 \
-#  -f dash -seg_duration 2 -frag_duration 1 \
-#  -hls_playlist true \
-#  -dash_segment_type webm \
-#  -c:v libvpx-vp9 -crf 30 -preset veryfast -g 30 -sc_threshold 0 \
-#  /www/thoughts/video/stream.mpd
 
