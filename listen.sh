edc441e9efdb31decec5ca00216778d5380d286e
David Lewis <duve.duve@gmail.com>
2020-09-09 19:33:36 -0600

    working again!

--- listen.sh
+++ listen.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ffmpeg \
-  -f rtp -i rtp://localhost:2021/testing
+  -f flv -listen 1 -i rtmp://localhost:2021/live/app \
   -c:a aac -b:a 24k -ac 1 \
   -c:v libx264 -profile:v baseline \
   -analyzeduration 0 \
   -probesize 32 \
   -level 3.0 -pix_fmt yuv420p \
-  -crf 23 -preset veryfast -g 30 -sc_threshold 0 \
+  -crf 17 -preset veryfast -g 30 -sc_threshold 0 \
   -use_template 1 \
   -seg_duration 1 \
   -frag_duration 1 \
