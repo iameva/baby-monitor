5c2f4bac0c36e4906bda36d4430f907ed9782b9d
David Lewis <duve.duve@gmail.com>
2020-09-24 20:46:51 -0600

    perms

--- pi/stream.sh
+++ pi/stream.sh
@@ -3,7 +3,7 @@ ffmpeg \
   -use_wallclock_as_timestamps 1 \
   -async 1 \
   -f video4linux2 -framerate 15 -video_size 640x360 -i /dev/video0 \
-  -f alsa -ac 1 -i hw:2 \
+  -f alsa -ac 1 -i hw:1 \
   -c:a aac -b:a 64k -ac 1 \
   -c:v libx264 \
   -crf 20 -preset veryfast \
