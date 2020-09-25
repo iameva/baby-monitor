954650b4540331399bcfc446f8a6b42283c411c4
David Lewis <duve.duve@gmail.com>
2020-09-24 22:27:10 -0600

    working everything!

--- architect/loop.sh
+++ architect/loop.sh
@@ -4,9 +4,10 @@ ROOT="$1"
 while true
 do
 	DATE="$(date '+%Y-%m-%d_%H:%M:%S')"
-	DIR="$ROOT/$DATE/video"
-	mkdir -p "$DIR"
+	DIR="$ROOT/$DATE"
+  VIDEO_DIR="$DIR/video"
+	mkdir -p "$VIDEO_DIR"
 	rm "$ROOT/live"
 	ln -s "$DIR/" "$ROOT/live"
-	sh "$SCRIPT_DIR/listen.sh" "$DIR"
+	sh "$SCRIPT_DIR/listen.sh" "$VIDEO_DIR"
 done
